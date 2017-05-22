da045602a209dd505b5093e32bd6e4f2008710ff
Michael Padget <mpadget@comcast.net>
2017-05-21 22:03:17 -0700

    Updated music model and routes

--- lab-padget/routes/music-routes.js
+++ lab-padget/routes/music-routes.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const Music = require('../models/music');
+
+module.exports = function(router) {
+  router.get('/api/music', (req, res) => {
+    if(!req.query.id) {
+      Music.find()
+      .then()
+      .catch();
+    } else {
+      Music.findById(req.params.id)
+      .then(music => {
+        console.log(music);
+        res.json(music);
+      })
+      .catch(err => res.status(404).send(err.message));
+    }
+  });
+
+  router.post('/api/music', (req, res) => {
+    new Music(req.body).save()
+    .then(music => {
+      console.log(music);
+      res.json(music);
+    })
+    .catch(err => res.status(400).send(err.message));
+  });
+
+  router.put('/api/music/:id', (req, res) => {
+    console.log(res);
+  });
+
+  router.delete('/api/music/:id', (req, res) => {
+    console.log(res);
+  });
+};
